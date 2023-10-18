b4363b715adc5e21cd47f3ec6e7f5c19eaa63c6f
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-10-18 01:38:43 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,70 @@
-"use client";
-
-import Image from "next/image";
+import { prismaClient } from "@/lib/prisma";
 import Categories from "./components/categories";
+import ProductList from "./components/product-list";
+import PromoBanner from "./components/promo-banner";
+import SectionTitle from "./components/section-title";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
-    <div className="p-5">
-      <Image
+    <div className="flex flex-col gap-8 py-8">
+      <PromoBanner
         src="/banner-home-01.png"
-        height={0}
-        width={0}
-        className="h-auto w-full"
-        sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8">
+      <div className="px-5">
         <Categories />
+      </div>
+
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
+        <ProductList products={deals} />
+      </div>
+
+      <PromoBanner
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em mouses!"
+      />
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
